7fbeecf4e9407d851f0f5a3d6a46f3163e3c6af4
kernel688 <jcmartinezcano@gmail.com>
2020-10-13 23:35:58 -0500

    migration from local storage to mongodb

--- src/app/basic-trx/basic-trx.component.ts
+++ src/app/basic-trx/basic-trx.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {dateOnly, dateTime, dateTimeStamp} from '../sharedFunctions'
+import { dateOnly, dateTime, dateTimeStamp } from '../sharedFunctions'
+import { HttprequestsService } from '../httprequests.service'
 
 @Component({
   selector: 'app-basic-trx',
@@ -8,33 +9,52 @@ import {dateOnly, dateTime, dateTimeStamp} from '../sharedFunctions'
 })
 export class BasicTrxComponent implements OnInit {
 
-  formData: any = {};
-  allTransactions: Array<object> = JSON.parse(localStorage.getItem("allTransactions")) || [];
-  currentBalance: number = Number(localStorage.getItem("currentBalance")) || 0;
-  currentTrxNumber: number = Number(localStorage.getItem("currentTrxNumber")) || 1;
-  
-  constructor() { }
-  
-  ngOnInit(): void {
+  constructor(public service: HttprequestsService) { 
+    
   }
   
-  addTrx() {
-    
+  
+  ngOnInit(): void {
+    this.getStarterInfo()
+  }
+  
+  
+  
+  formData: any = {}
+  allTransactions: any []
+  currentBalance: number = 0
+  
+  
+  async getStarterInfo() {
+    var result = await this.service.getTransactions()
+    if (result.result === true) {
+      this.allTransactions = result.data || []
+      if (this.allTransactions === []) {
+        this.currentBalance = 0
+      } else {
+        this.currentBalance = 0
+        this.allTransactions.forEach(trx => {
+          if (trx.type === 'Income') {
+            this.currentBalance = this.currentBalance + trx.amount
+          } else {
+            this.currentBalance = this.currentBalance - trx.amount
+          }
+        });
+      }
+      
+    } else {
+      alert(result.message)
+    }
+  }
+  
+  
+  async addTrx() {
     let validTrx = this.formData.amount > 0 && this.formData.trxType && this.formData.description
-
     dateTimeStamp()
     
     if (validTrx) {
-      let tempAmount = 0
-      this.formData.trxType === 'Expense'? tempAmount = this.formData.amount * -1 : tempAmount = this.formData.amount
-      this.currentBalance = tempAmount + this.currentBalance
-      localStorage.setItem("currentBalance",JSON.stringify(this.currentBalance))
-      
-      this.currentTrxNumber++
-      localStorage.setItem("currentTrxNumber",JSON.stringify(this.currentTrxNumber))
-      
       let trxDetails = {
-        number: this.currentTrxNumber,
+        number: Number(this.allTransactions.length) + 1 || 1,
         date: dateOnly,
         type: this.formData.trxType,
         amount: Number(this.formData.amount),
@@ -42,32 +62,55 @@ export class BasicTrxComponent implements OnInit {
         hidden: false,
         trxID: dateTime
       }
-      this.allTransactions.push(trxDetails)
-      localStorage.setItem("allTransactions",JSON.stringify(this.allTransactions))
-      
-      this.formData.amount = null
-      this.formData.trxType = null
-      this.formData.description = null
-      
-      
+
+      var result = await this.service.postTransaction(trxDetails)
+      if (result.result === true) {
+        this.formData.amount = null
+        this.formData.trxType = null
+        this.formData.description = null
+        this.getStarterInfo()
+      } else {
+        alert(result.message)
+      }
     } else {
       alert('Fill all the fields please!')
-      
     }
-    
-    
   }
+
+
+  async deleteTrx(transaction) {
+    var result = await this.service.deleteTransaction(transaction._id);
+    
+    if (result.result === true) {
+      this.getStarterInfo()
+      
+    } else {
+      alert(result.message)
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
   hideTrx(transaction) {
     let trxToHide = this.allTransactions[Number(this.allTransactions.indexOf(transaction))]
     trxToHide["hidden"] = true
 
     let tempAmount = 0
-    trxToHide["type"] === 'Income'? tempAmount = Number(trxToHide["amount"]) * -1 : tempAmount = Number(trxToHide["amount"])
+    trxToHide["type"] === 'Income' ? tempAmount = Number(trxToHide["amount"]) * -1 : tempAmount = Number(trxToHide["amount"])
     this.currentBalance = tempAmount + this.currentBalance
-    
-    localStorage.setItem("currentBalance",JSON.stringify(this.currentBalance))
-    localStorage.setItem("allTransactions",JSON.stringify(this.allTransactions))
+
+    localStorage.setItem("currentBalance", JSON.stringify(this.currentBalance))
+    localStorage.setItem("allTransactions", JSON.stringify(this.allTransactions))
   }
-  
+
 }
